ea88fb7b5a9d2935ac29b0e8b06693dbb2d50be0
cnerylozada <cnery@dynamicdevs.io>
2021-12-09 09:55:47 -0500

    feat: styles were created

---
+++ js/1.settings/color.js
@@ -0,0 +1,39 @@
+export const lightPalette = {
+  primary: "#070707",
+  danger: "#42a5f5",
+  green: "#0CC28C",
+  turquoise: "#CECECE",
+  blue: "#858585",
+  purple: "#F1F1F1",
+  lemon: "#a18eea",
+
+  text: "#223034",
+
+  bg1: "#f5fafd",
+  bg2: "#ffffff",
+  bg3: "#dfebf1",
+
+  gray1: "#f1f1f1",
+  gray2: "#cecece",
+  gray3: "#858585",
+};
+
+export const darkPalette = {
+  primary: "#00d9eb",
+  danger: "#ff381d",
+  green: "#00eea7",
+  turquoise: "#03d9cc",
+  blue: "#00a3ff",
+  purple: "#aa95ff",
+  lemon: "#88fd86",
+
+  text: "#ffffff",
+
+  bg1: "#25383e",
+  bg2: "#223034",
+  bg3: "#25383e",
+
+  gray1: "#f1f1f1",
+  gray2: "#48585e",
+  gray3: "#70858c",
+};
---
+++ js/1.settings/font.js
@@ -0,0 +1,24 @@
+export const fontFamily = "Lato, sans-serif";
+
+export const fontSize = {
+  "headline-desktop": "60px",
+  "headline-mobile": "36px",
+
+  "sectionHeadline-desktop": "30px",
+  "sectionHeadline-mobile": "24px",
+
+  "subHeadline-desktop": "24px",
+  "subHeadline-mobile": "18px",
+
+  "paragraph-desktop": "16px",
+  "paragraph-mobile": "14px",
+
+  "small-desktop": "12px",
+  "small-mobile": "12px",
+};
+
+export const fontWeight = {
+  bold: "700",
+  regular: "400",
+  light: "300",
+};
