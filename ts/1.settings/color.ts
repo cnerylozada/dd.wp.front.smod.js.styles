ac96f67dca1d88c54951532d938b7c8b74d5afc1
cnerylozada <cnery@dynamicdevs.io>
2021-12-09 19:42:37 -0500

    fix: light-palette colors

--- ts/1.settings/color.ts
+++ ts/1.settings/color.ts
@@ -1,39 +1,39 @@
 export const lightPalette = {
-  primary: "#070707",
-  danger: "#42a5f5",
-  green: "#0CC28C",
-  turquoise: "#CECECE",
-  blue: "#858585",
-  purple: "#F1F1F1",
-  lemon: "#a18eea",
+  primary: '#08c4d3',
+  danger: '#df1b00',
+  green: '#00cc8f',
+  turquoise: '#00c0b5',
+  blue: '#23a5ee',
+  purple: '#a18eea',
+  lemon: '#66e264',
 
-  text: "#223034",
+  text: '#223034',
 
-  bg1: "#f5fafd",
-  bg2: "#ffffff",
-  bg3: "#dfebf1",
+  bg1: '#f5fafd',
+  bg2: '#ffffff',
+  bg3: '#dfebf1',
 
-  gray1: "#f1f1f1",
-  gray2: "#cecece",
-  gray3: "#858585",
-};
+  gray1: '#f1f1f1',
+  gray2: '#cecece',
+  gray3: '#858585'
+}
 
 export const darkPalette = {
-  primary: "#00d9eb",
-  danger: "#ff381d",
-  green: "#00eea7",
-  turquoise: "#03d9cc",
-  blue: "#00a3ff",
-  purple: "#aa95ff",
-  lemon: "#88fd86",
+  primary: '#00d9eb',
+  danger: '#ff381d',
+  green: '#00eea7',
+  turquoise: '#03d9cc',
+  blue: '#00a3ff',
+  purple: '#aa95ff',
+  lemon: '#88fd86',
 
-  text: "#ffffff",
+  text: '#ffffff',
 
-  bg1: "#25383e",
-  bg2: "#223034",
-  bg3: "#25383e",
+  bg1: '#25383e',
+  bg2: '#223034',
+  bg3: '#25383e',
 
-  gray1: "#f1f1f1",
-  gray2: "#48585e",
-  gray3: "#70858c",
-};
+  gray1: '#f1f1f1',
+  gray2: '#48585e',
+  gray3: '#70858c'
+}
