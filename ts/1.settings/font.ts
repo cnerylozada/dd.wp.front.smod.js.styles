c18f3a50fd93825e54a234e2706a0462567ef794
cnerylozada <cnery@dynamicdevs.io>
2021-12-27 11:18:05 -0500

    fix: font-size

--- ts/1.settings/font.ts
+++ ts/1.settings/font.ts
@@ -1,24 +1,24 @@
-export const fontFamily = "Lato, sans-serif";
+export const fontFamily = 'Lato, sans-serif'
 
 export const fontSize = {
-  "headline-desktop": "60px",
-  "headline-mobile": "36px",
+  'headline-desktop': '60px',
+  'headline-mobile': '36px',
 
-  "sectionHeadline-desktop": "30px",
-  "sectionHeadline-mobile": "24px",
+  'sectionHeadline-desktop': '30px',
+  'sectionHeadline-mobile': '24px',
 
-  "subHeadline-desktop": "24px",
-  "subHeadline-mobile": "18px",
+  'subHeadline-desktop': '20px',
+  'subHeadline-mobile': '18px',
 
-  "paragraph-desktop": "16px",
-  "paragraph-mobile": "14px",
+  'paragraph-desktop': '16px',
+  'paragraph-mobile': '14px',
 
-  "small-desktop": "12px",
-  "small-mobile": "12px",
-};
+  'small-desktop': '12px',
+  'small-mobile': '12px'
+}
 
 export const fontWeight = {
-  bold: "700",
-  regular: "400",
-  light: "300",
-};
+  bold: '700',
+  regular: '400',
+  light: '300'
+}
